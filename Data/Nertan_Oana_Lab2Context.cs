365b6787e33fda6424079a3791f1ef527f7b0f18
maria <maria@MARIA>
2024-10-23 17:39:46 +0300

    Add project files.

---
+++ Data/Nertan_Oana_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nertan_Oana_Lab2.Models;
+
+namespace Nertan_Oana_Lab2.Data
+{
+    public class Nertan_Oana_Lab2Context : DbContext
+    {
+        public Nertan_Oana_Lab2Context (DbContextOptions<Nertan_Oana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nertan_Oana_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Nertan_Oana_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
