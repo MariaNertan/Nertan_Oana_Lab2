1f9db1f5427e8dd15e6c8c4bc395246f13474bbf
Maria Nertan <marianertan@yahoo.com>
2024-10-24 19:23:34 +0300

    Laborator3 complet, cu tot cu sarcina laborator

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -28,7 +28,12 @@ namespace Nertan_Oana_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book
+                .Include(b => b.Publisher)
+                .Include(b => b.BookCategories).ThenInclude(bc => bc.Category) // Adaugă categoriile
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            
             if (book == null)
             {
                 return NotFound();
