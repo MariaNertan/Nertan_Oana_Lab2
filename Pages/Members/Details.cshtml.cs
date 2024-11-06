235e11817555f0a47a13bb4485dd02aa916dee54
Maria Nertan <marianertan@yahoo.com>
2024-11-06 19:37:16 +0200

    E bine pana la ex 12 inclusiv

---
+++ Pages/Members/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Nertan_Oana_Lab2.Data;
+using Nertan_Oana_Lab2.Models;
+
+namespace Nertan_Oana_Lab2.Pages.Members
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Nertan_Oana_Lab2.Data.Nertan_Oana_Lab2Context _context;
+
+        public DetailsModel(Nertan_Oana_Lab2.Data.Nertan_Oana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Member Member { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FirstOrDefaultAsync(m => m.ID == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Member = member;
+            }
+            return Page();
+        }
+    }
+}
